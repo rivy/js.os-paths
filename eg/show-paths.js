43ea4cabc5ba9ee04e14d6fce34c8b78defd9063
Roy Ivy III <rivy.dev@gmail.com>
2019-10-02 22:10:12 -0500

    docs ~ add example script

---
+++ eg/show-paths.js
@@ -0,0 +1,24 @@
+// # spell-checker:ignore APPNAME
+/* eslint-env es6, node */
+'use strict';
+
+// # const path = require('path');
+const util = require('util');
+const _ = require('lodash') || undefined;
+
+const osPaths = require('..');
+
+console.log('osPaths:', util.inspect(osPaths));
+
+if (_) {
+	_.each(osPaths, (value, key) => {
+		console.log(key, '=', osPaths[key]());
+	});
+}
+
+process.env.TMPDIR = process.env.TEMP = process.TMP = 'temp'; 	// eslint-disable-line no-multi-assign
+if (_) {
+	_.each(osPaths, (value, key) => {
+		console.log(key, '=', osPaths[key]());
+	});
+}
