9a8d13216466b7f2bf537a73c2f54a6a11119122
Roy Ivy III <rivy.dev@gmail.com>
2020-12-12 17:08:46 -0600

    tests ~ remove duplicative code from type testing

--- test/index.test-d.ts
+++ test/index.test-d.ts
@@ -1,9 +1,7 @@
 import {expectType} from 'tsd';
 import osPaths = require('../src/lib');
 
-const paths: typeof osPaths = osPaths();
-
 expectType<typeof osPaths>(osPaths());
 
-expectType<string>(paths.home());
-expectType<string>(paths.temp());
+expectType<string>(osPaths.home());
+expectType<string>(osPaths.temp());
