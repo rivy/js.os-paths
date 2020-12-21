6a404291c3ade52b45a6b22a30b721a2082ff5a0
Roy Ivy III <rivy.dev@gmail.com>
2020-12-20 21:01:26 -0600

    fix `npx eslint test` complaints

--- test/unit.test.js
+++ test/unit.test.js
@@ -11,10 +11,12 @@ const osPaths = require('../build/umd');
 test('default', (t) => {
 	const paths = osPaths();
 
+	// eslint-disable-next-line functional/immutable-data
 	process.env.HOME = process.env.USERPROFILE = 'home';
+	// eslint-disable-next-line functional/immutable-data
 	process.env.TMPDIR = process.env.TEMP = process.env.TEMP = 'temp';
 
-	for (const key of Object.keys(paths)) {
+	Object.keys(paths).forEach((key) => {
 		const value = paths[key];
 		t.log(key, '=', value, '() =>', value());
 		const values = [].concat(value()); // # convert value (single value or array) to a flat array
@@ -22,30 +24,39 @@ test('default', (t) => {
 			values.reduce((a, v) => a && v === key, true),
 			true
 		);
-	}
+	});
 });
 
 test('alternate construction (via function)', (t) => {
 	const paths = osPaths();
+	// eslint-disable-next-line functional/immutable-data
 	process.env.HOME = process.env.USERPROFILE = 'home';
 	t.is(paths.home(), 'home');
 });
 
 test('no os.homedir/os.tmpdir', (t) => {
+	// eslint-disable-next-line functional/immutable-data
 	os.homedir = null;
+	// eslint-disable-next-line functional/immutable-data
 	os.tmpdir = null;
 	const paths = osPaths();
+	// eslint-disable-next-line functional/immutable-data
 	process.env.HOME = process.env.USERPROFILE = 'home';
+	// eslint-disable-next-line functional/immutable-data
 	process.env.TEMP = process.env.TMP = process.env.TMPDIR = 'temp';
 	t.is(paths.home(), 'home');
 	t.is(paths.temp(), 'temp');
 });
 
 test('no os.homedir/os.tmpdir and trailing slash in source', (t) => {
+	// eslint-disable-next-line functional/immutable-data
 	os.homedir = null;
+	// eslint-disable-next-line functional/immutable-data
 	os.tmpdir = null;
 	const paths = osPaths();
+	// eslint-disable-next-line functional/immutable-data
 	process.env.HOME = process.env.USERPROFILE = 'home/';
+	// eslint-disable-next-line functional/immutable-data
 	process.env.TEMP = process.env.TMP = process.env.TMPDIR = 'temp/';
 	t.is(paths.home(), 'home');
 	t.is(paths.temp(), 'temp');
