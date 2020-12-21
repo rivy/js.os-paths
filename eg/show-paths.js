71daafb4f3dcedb40629cddb3873e1fac9de6215
Roy Ivy III <rivy.dev@gmail.com>
2020-12-20 20:59:00 -0600

    docs ~ fix `npx eslint eg` complaints

--- eg/show-paths.js
+++ eg/show-paths.js
@@ -11,6 +11,7 @@ Object.keys(osPaths).forEach((key) => {
 	console.log({ key, value });
 });
 
+// eslint-disable-next-line functional/immutable-data
 process.env.TMPDIR = process.env.TEMP = process.env.TMP = 'temp';
 Object.keys(osPaths).forEach((key) => {
 	const value = osPaths[key]();
--- eg/show-paths.ts
+++ eg/show-paths.ts
@@ -12,6 +12,7 @@ Object.keys(osPaths).forEach((key) => {
 	console.log({ key, value });
 });
 
+// eslint-disable-next-line functional/immutable-data
 process.env.TMPDIR = process.env.TEMP = process.env.TMP = 'temp';
 Object.keys(osPaths).forEach((key) => {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
