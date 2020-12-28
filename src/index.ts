3481efd071f6b90ac20f58f45309f7a850b62309
Roy Ivy III <rivy.dev@gmail.com>
2020-12-28 01:07:40 -0600

    change/API! ~ add ESM module packaging (adds 'exports' to package)
    - adds an ESM wrapper to provide simple ESM support (as suggested in "Node Modules at War"
    
    * ref: [Node Modules at War](https://redfin.engineering/node-modules-at-war-why-commonjs-and-es-modules-cant-get-along-9617135eeca1) @@ <https://archive.is/zl0qg>
    * ref: [yargs ~ 'Road to ESM/Deno'](https://github.com/yargs/yargs/issues/1706)

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,8 @@ const haveModuleExports_ = typeof module === 'object' && module.exports;
 // ## maint ~ [2020-12-23; rivy] although tested, `nyc` is unable to instrument ESM/.mjs correctly in order show coverage for testing the *else* clause
 /* istanbul ignore else */
 if (haveModuleExports_) {
-	// enables direct require from CJS (eg, `const m = require('...');`); skipped for ESM
+	// enables direct require from CJS (eg, `const module = require('...');`), but generally disables any other exports
+	// * skipped for ESM (missing `module.exports`)
 	// eslint-disable-next-line functional/immutable-data
 	module.exports = default_;
 }
