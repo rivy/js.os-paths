d78b19d07f405d96248aa41f3d3fb52d94a9153e
Roy Ivy III <rivy.dev@gmail.com>
2020-12-28 01:34:24 -0600

    fix code coverage for current problems with instrumenting ESM modules under `nyc`

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+// spell-checker:ignore maint rivy
 import { default as default_ } from './lib/OSPaths';
 
 export * from './lib/OSPaths';
+
 export default default_;
 
-if (module) {
+const haveModuleExports_ = typeof module === 'object' && module.exports;
+// ## maint ~ [2020-12-23; rivy] although tested, `nyc` is unable to instrument ESM/.mjs correctly in order show coverage for testing the *else* clause
+/* istanbul ignore else */
+if (haveModuleExports_) {
+	// enables direct require from CJS (eg, `const m = require('...');`); skipped for ESM
 	// eslint-disable-next-line functional/immutable-data
 	module.exports = default_;
 }
