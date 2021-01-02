14b271870233176afb460e7b4e720847bcbd685b
Roy Ivy III <rivy.dev@gmail.com>
2021-01-01 22:02:04 -0600

    change ~ add '_esm!' property to export for CJS as further easy hack to access all exports

--- src/index.ts
+++ src/index.ts
@@ -16,4 +16,11 @@ if (haveModuleExports_) {
 	// * added non-enumerable '_esm!' property (as a hack) to allow full access to all exports (for testing, ...)
 	// eslint-disable-next-line functional/immutable-data
 	module.exports = default_;
+	// add a non-enumerable '_esm!' property (really a hack) to allow full access to all exports (for testing, ...)
+	Object.defineProperty(module.exports, '_esm!', {
+		get() {
+			return exports;
+		},
+		enumerable: false,
+	});
 }
