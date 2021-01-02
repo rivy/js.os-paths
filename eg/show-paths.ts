9aeffc0d296b6ed23b7ce7f4b6c4c78be599f0f0
Roy Ivy III <rivy.dev@gmail.com>
2021-01-02 10:12:51 -0600

    docs ~ polish TS example

--- eg/show-paths.ts
+++ eg/show-paths.ts
@@ -1,10 +1,8 @@
 import osPaths from '../src';
 
-/* eslint-disable no-console , functional/immutable-data , security/detect-object-injection, security-node/detect-crlf */
+/* eslint-disable no-console , functional/immutable-data , security/detect-object-injection, security-node/detect-crlf , @typescript-eslint/no-explicit-any */
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function objectEntries(obj: any) {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const map: any = {};
 	Object.keys(obj).forEach((key) => {
 		const value = obj[key];
@@ -20,4 +18,4 @@ console.log(objectEntries(osPaths));
 process.env.TMPDIR = process.env.TEMP = process.env.TMP = 'temp';
 console.log(objectEntries(osPaths));
 
-/* eslint-enable no-console , functional/immutable-data , security/detect-object-injection, security-node/detect-crlf */
+/* eslint-enable no-console , functional/immutable-data , security/detect-object-injection, security-node/detect-crlf , @typescript-eslint/no-explicit-any*/
