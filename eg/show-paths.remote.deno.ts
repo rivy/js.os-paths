2bdd8f01b5fff636c9879ccaa102820b56142e8d
Roy Ivy III <rivy.dev@gmail.com>
2021-01-07 21:52:09 -0600

    fix ~ remove `/// <reference path...` references to deno types
    ## [why]
    
    Deno barfs to a halt with type conflict errors when it's not exactly in sync with the
    otherwise referenced types, which is not controllable by the repository. So, remove any
    references to Deno types from the TS files. Intellisense is broken by this but the code
    will now run.
    
    Both the built-in JS/TS plugin (vscode.typescript-language-features) and the
    Deno-specific plugin (denoland.vscode-deno) are currently unable to understand and work
    within a mixed repository.
    
    Await better tooling.

--- eg/show-paths.remote.deno.ts
+++ eg/show-paths.remote.deno.ts
@@ -3,9 +3,6 @@
 
 /* eslint-disable no-console , security/detect-object-injection, security-node/detect-crlf , @typescript-eslint/ban-ts-comment , @typescript-eslint/no-explicit-any */
 
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../src/types/deno.d.ts"/>
-
 // @ts-ignore
 import osPaths from 'https://cdn.jsdelivr.net/gh/rivy/js.os-paths@5b6b93bdb048098b86f9b8f0952180c2bf0532b1/src/mod.deno.ts';
 // import osPaths from 'https://cdn.jsdelivr.net/gh/rivy/js.os-paths@2d5924e692617a575c8a73c9fb611e93085c4339/src/mod.deno.ts';
--- src/platform-adapters/deno.deno.ts
+++ src/platform-adapters/deno.deno.ts
@@ -1,21 +1,25 @@
 // spell-checker:ignore Deno
 
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../types/deno.d.ts"/>
-
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import * as path from 'https://deno.land/std@0.81.0/path/mod.ts';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
 // @ts-ignore
 import { Platform } from './_base.ts';
 
+// create a local reference to refer to `Deno` (for better linting without need for multiple `// @ts-ignore` directives)
+// @ts-ignore
+const deno = Deno;
+
 export const adapter: Platform.Adapter = {
-	env: { get: Deno.env.get },
+	env: { get: deno.env.get },
 	// Deno (as of v1.6) has no built-in implementation for homedir() or tmpdir()
 	os: {}, // * module is tolerant of missing homedir()/tmpdir() functions
-	meta: { mainFilename: Deno.mainModule },
+	meta: { mainFilename: deno.mainModule },
 	path,
-	process: { platform: Deno.build.os },
+	process: { platform: deno.build.os },
 };
+
+/* eslint-enable @typescript-eslint/ban-ts-comment */
