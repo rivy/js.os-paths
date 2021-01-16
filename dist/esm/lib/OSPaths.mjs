b5edf8f5ec7d6082ae7b56659b4b474390003103
Roy Ivy III <rivy.dev@gmail.com>
2021-01-16 17:34:19 -0600

    maint/dist ~ update

--- dist/esm/lib/OSPaths.mjs
+++ dist/esm/lib/OSPaths.mjs
@@ -3,30 +3,22 @@ import { __spreadArrays } from '../_virtual/_tslib.js';
 function isEmpty(s) {
     return !s;
 }
-function OSPathsAdaptionBuilder_(adapter_) {
-    var env = adapter_.env, os = adapter_.os, path = adapter_.path, process = adapter_.process;
-    var isWinOS = /^win/i.test(process.platform);
-    function normalizePath(path_) {
-        return path_ ? path.normalize(path.join(path_, '.')) : void 0;
-    }
-    var posix = function () {
-        var home = function () {
+var Adapt;
+(function (Adapt) {
+    Adapt.isWinOS = function (adapter_) { return /^win/i.test(adapter_.process.platform); };
+    Adapt.normalizePath = function (adapter_) {
+        return function (path_) {
+            return path_ ? adapter_.path.normalize(adapter_.path.join(path_, '.')) : void 0;
+        };
+    };
+    Adapt.home = function (adapter_) {
+        var env = adapter_.env, os = adapter_.os, path = adapter_.path;
+        var isWinOS = Adapt.isWinOS(adapter_);
+        var normalizePath = Adapt.normalizePath(adapter_);
+        var posix = function () {
             return normalizePath((typeof os.homedir === 'function' ? os.homedir() : void 0) || env.get('HOME'));
         };
-        var temp = function () {
-            var fallback = '/tmp';
-            var priorityList = [
-                typeof os.tmpdir === 'function' ? os.tmpdir() : void 0,
-                env.get('TMPDIR'),
-                env.get('TEMP'),
-                env.get('TMP'),
-            ];
-            return normalizePath(priorityList.find(function (v) { return !isEmpty(v); })) || fallback;
-        };
-        return { home: home, temp: temp };
-    };
-    var windows = function () {
-        var home = function () {
+        var windows = function () {
             var priorityList = [
                 typeof os.homedir === 'function' ? os.homedir() : void 0,
                 env.get('USERPROFILE'),
@@ -37,17 +29,33 @@ function OSPathsAdaptionBuilder_(adapter_) {
             ];
             return normalizePath(priorityList.find(function (v) { return !isEmpty(v); }));
         };
+        return isWinOS ? windows : posix;
+    };
+    Adapt.temp = function (adapter_) {
+        var env = adapter_.env, os = adapter_.os, path = adapter_.path;
+        var isWinOS = Adapt.isWinOS(adapter_);
+        var normalizePath = Adapt.normalizePath(adapter_);
         function joinPathToBase(base, segments) {
             return base ? path.join.apply(path, __spreadArrays([base], segments)) : void 0;
         }
-        var temp = function () {
+        var posix = function () {
+            var fallback = '/tmp';
+            var priorityList = [
+                typeof os.tmpdir === 'function' ? os.tmpdir() : void 0,
+                env.get('TMPDIR'),
+                env.get('TEMP'),
+                env.get('TMP'),
+            ];
+            return normalizePath(priorityList.find(function (v) { return !isEmpty(v); })) || fallback;
+        };
+        var windows = function () {
             var fallback = 'C:\\Temp';
             var priorityListLazy = [
                 os.tmpdir,
                 function () { return env.get('TEMP'); },
                 function () { return env.get('TMP'); },
                 function () { return joinPathToBase(env.get('LOCALAPPDATA'), ['Temp']); },
-                function () { return joinPathToBase(home(), ['AppData', 'Local', 'Temp']); },
+                function () { return joinPathToBase(Adapt.home(adapter_)(), ['AppData', 'Local', 'Temp']); },
                 function () { return joinPathToBase(env.get('ALLUSERSPROFILE'), ['Temp']); },
                 function () { return joinPathToBase(env.get('SystemRoot'), ['Temp']); },
                 function () { return joinPathToBase(env.get('windir'), ['Temp']); },
@@ -56,21 +64,22 @@ function OSPathsAdaptionBuilder_(adapter_) {
             var v = priorityListLazy.find(function (v) { return v && !isEmpty(v()); });
             return (v && normalizePath(v())) || fallback;
         };
-        return { home: home, temp: temp };
+        return isWinOS ? windows : posix;
     };
-    var OSPaths_ = (function () {
-        function OSPaths_() {
-            var OSPaths = function () {
-                return new OSPaths_();
-            };
-            var platformOS = isWinOS ? windows() : posix();
-            OSPaths.home = platformOS.home;
-            OSPaths.temp = platformOS.temp;
-            return OSPaths;
-        }
-        return OSPaths_;
-    }());
-    return new OSPaths_();
+})(Adapt || (Adapt = {}));
+function OSPathsAdaptionBuilder_(adapter_) {
+    function OSPaths() {
+        return obj;
+    }
+    var home = Adapt.home(adapter_);
+    var temp = Adapt.temp(adapter_);
+    Object.defineProperty(home, 'name', { value: 'home' });
+    Object.defineProperty(temp, 'name', { value: 'temp' });
+    var obj = Object.assign(OSPaths, {
+        home: home,
+        temp: temp,
+    });
+    return obj;
 }
 
 export { OSPathsAdaptionBuilder_ };
--- dist/types/lib/OSPaths.d.ts
+++ dist/types/lib/OSPaths.d.ts
@@ -1,8 +1,13 @@
 import { Platform } from '../platform-adapters/_base';
+/** Determine common OS/platform paths (home, temp, ...) */
 export declare type OSPaths = {
-    new (): OSPaths;
+    /** @constructor Create an `OSPaths` object. */
     (): OSPaths;
-    readonly home: () => string | undefined;
-    readonly temp: () => string;
+    /** @constructor Create an `OSPaths` object. */
+    new (): OSPaths;
+    /** Returns the path string of the user's home directory (or `undefined`if the user's home directory is not resolvable). */
+    home(): string | undefined;
+    /** Returns the path string of the system's default directory for temporary files. */
+    temp(): string;
 };
 export declare function OSPathsAdaptionBuilder_(adapter_: Platform.Adapter): OSPaths;
