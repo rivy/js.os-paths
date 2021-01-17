ddaeb97e87117fab2c4348c3465ce3756011f588
Roy Ivy III <rivy.dev@gmail.com>
2021-01-17 17:50:03 -0600

    docs ~ update Deno examples

--- eg/show-paths.remote.CDN.deno.ts
+++ eg/show-paths.remote.CDN.deno.ts
@@ -4,7 +4,9 @@
 /* eslint-disable no-console , functional/immutable-data , security/detect-object-injection, security-node/detect-crlf , @typescript-eslint/ban-ts-comment , @typescript-eslint/no-explicit-any */
 
 // @ts-ignore
-import osPaths from 'https://deno.land/x/os_paths@v6.0.0/src/mod.deno.ts';
+import osPaths from 'https://cdn.jsdelivr.net/gh/rivy/js.os-paths@2d5924e692617a575c8a73c9fb611e93085c4339/src/mod.deno.ts';
+// import osPaths from 'https://cdn.jsdelivr.net/gh/rivy/js.os-paths@latest/src/mod.deno.ts';
+// import osPaths from 'https://cdn.jsdelivr.net/gh/rivy/js.os-paths@6.0/src/mod.deno.ts';
 
 // create a local reference to refer to `Deno` (for better linting without need for multiple `// @ts-ignore` directives)
 // @ts-ignore
