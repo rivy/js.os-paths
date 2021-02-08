e4c692de8132b63375c91146ce9a7b32ea559705
Roy Ivy III <rivy.dev@gmail.com>
2021-02-07 23:30:26 -0600

    docs ~ add CJS example with dynamic require (using package 'main')

---
+++ eg/show-paths.dynamic.cjs.js
@@ -0,0 +1,25 @@
+/* eslint-env es6, node */
+'use strict';
+
+// eslint-disable-next-line security-node/detect-non-literal-require-calls , security/detect-non-literal-require
+const osPaths = require('../' + require('../package.json').main);
+
+/* eslint-disable no-console , functional/immutable-data , security/detect-object-injection, security-node/detect-crlf */
+
+function objectEntries(obj) {
+	const map = {};
+	Object.keys(obj).forEach((key) => {
+		const value = obj[key];
+		const val = typeof value === 'function' ? value() : value;
+		map[key] = val;
+	});
+	return map;
+}
+
+console.log({ osPaths });
+console.log(objectEntries(osPaths));
+
+process.env.TMPDIR = process.env.TEMP = process.env.TMP = 'temp';
+console.log(objectEntries(osPaths));
+
+/* eslint-enable no-console, functional/immutable-data , security/detect-object-injection , security-node/detect-crlf */
