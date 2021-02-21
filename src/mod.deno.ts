2aa4c5278ed4ff607447330bf6f75e88b229e958
Roy Ivy III <rivy.dev@gmail.com>
2021-02-21 16:15:12 -0600

    maint/build ~ revise CJS type rewrite to handle unnamed default export

--- src/mod.deno.ts
+++ src/mod.deno.ts
@@ -5,7 +5,7 @@
 import { Adapt } from '../dist/esm/lib/OSPaths.js';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import type OSPaths from '../dist/types/mod.d.ts';
+import type { OSPaths } from '../dist/types/mod.d.ts';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
