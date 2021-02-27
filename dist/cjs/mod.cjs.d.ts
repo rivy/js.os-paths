da7d14f936b7cab68e7d5aba6e6779759c522e55
Roy Ivy III <rivy.dev@gmail.com>
2021-02-27 12:47:43 -0600

    6.8.0

--- dist/cjs/mod.cjs.d.ts
+++ dist/cjs/mod.cjs.d.ts
@@ -1,9 +1,8 @@
 /** `OSPaths` (API) Determine common OS/platform paths (home, temp, ...) */
 interface OSPaths {
-    /** Create an `OSPaths` object (`new` is optional). */
-    new (): OSPaths;
-    /** Create an `OSPaths` object (`new` is optional). */
+    /** Create an `OSPaths` object (preceding `new` is optional). */
     (): OSPaths;
+    new (): OSPaths;
     /** Returns the path string of the user's home directory (or `undefined` if the user's home directory is not resolvable). */
     home(): string | undefined;
     /** Returns the path string of the system's default directory for temporary files. */
