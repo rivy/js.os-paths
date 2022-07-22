e529af450c0cd0a77ff39602a7d41776c109c17d
Roy Ivy III <rivy.dev@gmail.com>
2022-07-22 09:24:27 -0500

    maint/dev ~ update Prettier configuration (polish)

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,9 @@
+// Prettier configuration
+// ref: <https://prettier.io/docs/en/options.html>
+// v2022-07-13 [rivy]
+
+// spell-checker:ignore (people) Roy Ivy III * rivy
+
 module.exports = {
 	// $schema: 'http://json.schemastore.org/prettierrc',
 	printWidth: 100,
@@ -5,6 +11,14 @@ module.exports = {
 	singleQuote: true,
 	useTabs: true,
 	tabWidth: 2,
-	// ## overrides/[*.markdown]/tabWidth": "// set this to 4 when/if https://github.com/prettier/prettier/issues/5019 is fixed",
-	overrides: [{ files: ['*.md', '*.mkd', '*.markdown'], options: { tabWidth: 2, useTabs: false } }],
+	// "# overrides/[*.markdown]/tabWidth": "// set to 4 when https://github.com/prettier/prettier/issues/5019 is fixed",
+	overrides: [
+		{
+			files: ['*.md', '*.mkd', '*.markdown'],
+			options: {
+				tabWidth: 2,
+				useTabs: false,
+			},
+		},
+	],
 };
