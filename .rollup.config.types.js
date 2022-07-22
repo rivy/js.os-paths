875e8c00e52d9abbec6cf84f58c920aef5f300e1
Roy Ivy III <rivy.dev@gmail.com>
2022-07-22 09:25:00 -0500

    maint/dev ~ update `rollup` configuration (polish)

--- .rollup.config.types.js
+++ .rollup.config.types.js
@@ -1,7 +1,9 @@
 // `rollup` configuration
 // ref: <https://devhints.io/rollup>
-// v2002-07-13 [rivy]
+// v2002-07-14 [rivy]
 // setup: `npm i rollup @rollup/plugin-typescript` or `npm i rollup rollup-plugin-typescript2` (for visible TS error output)
+
+// spell-checker:ignore (people) Roy Ivy III * rivy
 
 import dts from 'rollup-plugin-dts';
 
@@ -18,5 +20,6 @@ export default [
 		input: './build/types/src/mod.cjs.d.ts',
 		output: [{ file: './dist/types/mod.cjs.d.ts', format: 'cjs' }],
 		plugins: [dts()],
+		// * note: for correct interpretation of types by VSCode, './dist/types/mod.cjs.d.ts' requires a subsequent text replacement ("export { _default as default }" => "export = _default")
 	},
 ];
